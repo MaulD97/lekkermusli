276318b995de9a88cb309c2d6e8e72c0c14c6989
MaulD97 <maikd97@hotmail.de>
2016-11-11 09:55:38 +0100

    ich bin so dunn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 $("<nav class=\'navbar\'><div class=\'container-fluid\'><ul class=\'nav navbar-nav\'><li><a href=\'index.html\'>Home</a></li><li class=\'dropdown\'><a class=\'dropdown-toggle\' data-toggle=\'dropdown\' href=\'statistiken.html\'>Statistiken<span class=\'caret\'></span></a><ul class=\'dropdown-menu\'><li><a href=\'statistiken.html\'>5geg5</a></li><li><a href=\'#\'>3geg3</a></li><li><a href=\'#\'>Einzelergebnisse</a></li></ul></li><li><a href=\'#\'>Stories</a></li><li><a href=\'#\'>Profile</a></li></ul></div></nav>").prependTo("body");
 $(".navbar").after("<div class=\'jumbotron\'><div class=\'container\'><h1 id=\'überschrift\'>Die neue Müsli Seite</h1><p>Wir haben die Seite aus Designgründen überarbeitet. Diese Seite verwendet nun Bootstrap für ein dynamisches Webpagevergnügen.</p><a href=\'https://mauld97.github.io/teammusli/\' target=\'_blank\' class=\'btn btn-primary btn-lg\' id=\'butt\'>Zur alten >></a></div></div>");
-$("<link rel=\'shortcut icon\' href=\'meida/müslibot2.ico\'>")prependTo("head");
+$("<link rel=\'shortcut icon\' href=\'meida/müslibot2.ico\'>").prependTo("head");
 });
