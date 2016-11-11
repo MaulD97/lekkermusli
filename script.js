f67ae6b9bbedd66c27c71b9b5134480678b1375b
MaulD97 <maikd97@hotmail.de>
2016-11-11 08:52:14 +0100

    gege+

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 $(document).read(function(){
 $("<nav class=\'navbar\'><div class=\'container-fluid\'><ul class=\'nav navbar-nav\'><li><a href=\'#\'>Home</a></li><li class=\'dropdown\'><a class=\'dropdown-toggle\' data-toggle=\'dropdown\' href=\'#\'>Statistiken<span class=\'caret\'></span></a><ul class=\'dropdown-menu\'><li><a href=\'#\'>5geg5</a></li><li><a href=\'#\'>3geg3</a></li><li><a href=\'#\'>Einzelergebnisse</a></li></ul></li><li><a href=\'#\'>Stories</a></li><li><a href=\'#\'>Profile</a></li></ul></div></nav>").prependTo("body");
-}):
+});
