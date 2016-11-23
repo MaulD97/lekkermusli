7b8df3147ff9ef3d7cdb33ac596889530a7cacdd
MaulD97 <maikd97@hotmail.de>
2016-11-23 14:39:49 +0100

    php eingeführt

---
+++ statistiken.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Team Musli | Statistiken</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel='stylesheet' href='bootstrap.css'/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="script.js" type="text/javascript"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+               <?php
+
+                    $row = 1;
+                    if (($handle = fopen("irgendwas.csv", "r")) !== FALSE) {
+
+                        echo '<table border="1">';
+
+                        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+                            $num = count($data);
+                            if ($row == 1) {
+                                echo '<thead><tr>';
+                            }else{
+                                echo '<tr>';
+                            }
+
+                            for ($c=0; $c < $num; $c++) {
+                                //echo $data[$c] . "<br />\n";
+                                if(empty($data[$c])) {
+                                   $value = "&nbsp;";
+                                }else{
+                                   $value = $data[$c];
+                                }
+                                if ($row == 1) {
+                                    echo '<th>'.$value.'</th>';
+                                }else{
+                                    echo '<td>'.$value.'</td>';
+                                }
+                            }
+
+                            if ($row == 1) {
+                                echo '</tr></thead><tbody>';
+                            }else{
+                                echo '</tr>';
+                            }
+                            $row++;
+                        }
+
+                        echo '</tbody></table>';
+                        fclose($handle);
+                    } 
+                    ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
